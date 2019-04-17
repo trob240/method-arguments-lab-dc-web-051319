b8d6a59ea36b9f1033023da78c3696f4241e9b06
Trevahr Robertson <trevahr@yahoo.com>
2019-04-17 04:43:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,5 +4,9 @@ end
 
 
 def introduction_with_language(name,language)
+<<<<<<< HEAD
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+=======
+  
+>>>>>>> 5f09a142542740545d09e6ac42d69105002e133e
 end
